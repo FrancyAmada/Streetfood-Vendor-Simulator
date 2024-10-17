b5823a3fb173cdb58015d79deaf1b1b8c856997c
Jose <joseearronn@gmail.com>
2024-10-18 01:44:41 +0800

    add restock menu base

---
+++ scenes/restock_menu/item.gd
@@ -0,0 +1,45 @@
+extends VBoxContainer
+class_name Item
+
+signal buy_item(item_name)
+
+@onready var item_label = $ItemLabel
+@onready var price_label = $PriceLabel
+@onready var stocks_label = $StocksLabel
+@onready var buy_button = $BuyButton
+
+@export var item_name: String = ""
+@export var price: int = 0
+@export var pack_size: int = 0
+@export var locked: bool = false
+
+var current_stocks: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	item_label.text = item_name
+	price_label.text = str(price)
+	update_stocks_label()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func update_stocks_label() -> void:
+	if locked: 
+		buy_button.text = "UNLOCK"
+	stocks_label.text = "Current Stocks: " + str(current_stocks)
+
+func unlock() -> void:
+	locked = false
+	buy_button.text = "BUY"
+
+func buy() -> void:
+	if locked:
+		unlock()
+	else:
+		current_stocks += pack_size
+		update_stocks_label()
+
+func _on_buy_button_button_down() -> void:
+	emit_signal("buy_item", item_name)
---
+++ scenes/restock_menu/restock_menu.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var items = $Items.get_children()
+@onready var oil = $Oil
+@onready var money_label = $MoneyLabel
+
+@export var money: int = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	items.map(func(item): item.connect("buy_item", _on_buy_item))
+	oil.connect("refill_oil", _on_refill_oil)
+	update_money_label()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func update_money_label() -> void:
+	money_label.text = str(money)
+
+func _on_buy_item(item_name: String) -> void:
+	for item in items:
+		if item.item_name == item_name:
+			if money < item.price:
+				print("NOT ENOUGH MONEY")
+				return
+			money -= item.price
+			item.buy()
+			update_money_label()
+			
+func _on_refill_oil() -> void:
+	if money < oil.price:
+		print("NOT ENOUGH MONEY")
+		return
+	if oil.oil_level >= 100:
+		print("MAXIMUM CAPACITY")
+		return
+	money -= oil.price
+	oil.buy()
+	update_money_label()
+	
+func _on_next_button_button_down() -> void:
+	print("NEXT")
