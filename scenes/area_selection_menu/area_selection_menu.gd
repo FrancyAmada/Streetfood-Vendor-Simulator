2043a6aca33fb27c5513cbe03225729ae4af9258
code1426 <gmver27@gmail.com>
2024-10-19 00:33:48 +0800

    Added area Selection menu

---
+++ scenes/area_selection_menu/area_selection_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_street_pressed() -> void:
+	print("Street pressed")
+
+
+func _on_school_pressed() -> void:
+	print("School pressed")
+
+
+func _on_festive_pressed() -> void:
+	print("Festive pressed")
